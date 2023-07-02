db1c8e117e52916feb96176ef3234511b098927b
queensk <queenskisivuli@gmail.com>
2023-07-02 18:32:50 +0300

    main server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import routes from './routes/index.js';
+
+const app = express();
+
+app.use(routes);
+
+const port = process.env.PORT || 5000;
+
+/**
+ * Start listening on the port
+ * @param {number} port - The port number to listen on
+ * @param {function} callback - The callback function to execute after listening
+ */
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
